c42da09b9c4f128a55ea7a2a91a9c55df2f6d308
root <root@watchdogddl-ms-7788.home>
2019-09-10 10:23:20 +0200

    initial version

---
+++ models/product_pricelist.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# from odoo import api, fields, models, tools, _
+#
+#
+# class Pricelist(models.Model):
+#     _inherit = "product.pricelist"
+#
+#     user_ids = fields.Many2many('res.users', 'res_pricelist_users_rel', 'pricelist_id', 'user_id',
+#                                 string='Accepted Users')
---
+++ models/user.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+import logging
+
+
+class Users(models.Model):
+    _inherit = "res.users"
+
+    pricelist_ids = fields.Many2many('product.pricelist', string='Pricelist')
+
